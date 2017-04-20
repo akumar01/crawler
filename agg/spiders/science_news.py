e3bc020e939f336518ce52c7ad42647b97cc0462
akumar01 <akumar.4db@gmail.com>
2017-04-19 21:25:52 -0700

    finished work on science_news, need to troubleshoot now

--- agg/spiders/science_news.py
+++ agg/spiders/science_news.py
@@ -1,6 +1,8 @@
 import scrapy 
 import datetime
 import pdb
+from lxml import etree
+from io import StringIO
 from ..items import JournalArticle, JournalArticleHTML
 
 class ScienceNewsSpider(scrapy.Spider):
@@ -11,6 +13,7 @@ class ScienceNewsSpider(scrapy.Spider):
 			self.sync_length = int(kwargs['sync_length'])
 		except Exception as e:
 			self.sync_length = 10
+		self.base_url = 'https://www.sciencemag.org'
 
 	def start_requests(self):
 		urls = ['http://www.sciencemag.org/news/latest-news']
@@ -40,7 +43,13 @@ class ScienceNewsSpider(scrapy.Spider):
 
 			news_article = parse_science_article(article, news_article)
 
+			# The date format is particular to the source
+			# Consult the bottom of the datetime module documentation 
+			article_date = datetime.datetime.strptime(article["date"], '%b %d, %Y')
+			article_dates.append(article_date)
 
+			# If the article is older than the time period we are interested in, 
+			# ignore it. 
 
 			article_date = datetime.datetime.strptime(article["date"], '%Y-%m-%d')
 			article_dates.append(article_date)
@@ -53,7 +62,7 @@ class ScienceNewsSpider(scrapy.Spider):
 				# parse its contents
 #				scrapy.Request(url = link, callback = )
 				if news_article["html_src"]:
-					news_article["html_src"] = response.urljoin(news_article["html_src"])
+					news_article["html_src"] = self.base_url + news_article["html_src"]
 					yield scrapy.Request(url = news_article["html_src"], 
 										callback = self.retrieve_article, meta = {"item":news_article})
 				else:
@@ -62,7 +71,7 @@ class ScienceNewsSpider(scrapy.Spider):
 
 		if min(article_dates) > datetime.datetime.utcnow()\
 								- datetime.timedelta(self.sync_length):
-			link = response.urljoin(self.get_older_url(response, article_date))
+			link = self.base_url + self.get_older_url(response)
 			pdb.set_trace()
 			yield scrapy.Request(url = link, callback = self.parse, 
 								 meta = {'pageno':response.meta['pageno'] + 1})
@@ -83,10 +92,49 @@ class ScienceNewsSpider(scrapy.Spider):
 
 		# Get the article date
 		date = article.css('time::text').extract_first()
-		# Remove period from the month contraction:
-		date2 = date.split()
-		date2[0] = date2[0][-1]
-		date = date2[0] + ' ' date2[1] + ' ' date2[2]
+
+		# Science does not put leading zeros on their days. Fix this:
+		date_split = date.split()
+
+		date_split[1] = '%02d' % int(date_split[1][:-1])
+
+		# Also get rid of the period at the end of the month
+		date = date_split[0][:-1] + ' ' + date_split[1] + ' ' + date_split[2]
+
 		news_article["date"] = date
 
-		# Get the 
+		# tags are retrieved from the article itself
+		
+		# This is a relative url that needs to be modified subsequently
+		news_article["html_src"] = article.css('h2.media_headline').css('a::attr(href)').extract_first()
+
+		return news_article
+
+	def retrieve_article(self, response):
+		# Retrieve the html of the article body. Subsequently we will convert it to a pdf:
+		# We need to parse this body and replace relative links with their full forms
+		item = response.meta["item"]
+		article_body = response.css('div.article__body').extract_first()
+
+		html_parser = etree.HTMLParser()
+		html_tree = etree.parse(StringIO(article_body), html_parser)
+
+		links = html_tree.findall('//a[@href]')
+		link += html_tree.findall('//*[@src]')
+		for link in links:
+			attr = 'src' if 'src'in link.keys() else 'href'
+			url = link.get(attr)
+			# Currently only aware of relative links to within the sciencemag domain. 
+			if(url.startswith(r'/')):
+				url = self.base_url + url
+
+			link.set(attr, url)
+
+		item["article_html"] = etree.tostring(html_tree.getroot(), pretty_print = True, method='html')
+
+		return item
+
+	def get_older_url(self, response):
+		# Science conveniently has a next page button that we can find regardless
+		# of the current page
+		return response.xpath("//a[@title='Go to next page']").css('a::attr(href)').extract_first()
