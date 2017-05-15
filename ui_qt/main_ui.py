1b2113c5278d0f06693feef1e0edac73bd010a90
akumar01 <akumar.4db@gmail.com>
2017-05-15 03:49:32 -0700

    Switching to pyqt

--- ui_qt/main_ui.py
+++ ui_qt/main_ui.py
@@ -1,8 +1,10 @@
 import sys
+import os
 from win32api import GetSystemMetrics
 from PyQt5.QtWidgets import (QApplication, QVBoxLayout, QHBoxLayout, QWidget,
 							 QLabel, QPushButton, QScrollArea, QGridLayout)
 from crawler.agg.json_out import read_json
+from crawler.project_vars import Paths, Spiders
 import pdb
 
 lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum tempor tortor, vitae molestie lorem mattis et. Mauris consectetur massa non metus blandit scelerisque vestibulum nec augue. Donec eu venenatis dui. Praesent consectetur facilisis justo, quis porta odio convallis sit amet. Praesent congue quam eros, malesuada feugiat velit lacinia sit amet. Integer condimentum in nibh consectetur accumsan. Aliquam erat volutpat. Morbi eget vulputate dolor, quis malesuada nisl. Ut quis tincidunt purus, quis cursus augue. Vivamus vitae pellentesque elit, a sagittis nulla. Praesent varius, magna sit amet blandit porttitor, leo mauris sodales risus, varius vehicula quam elit vitae odio.'
@@ -16,12 +18,25 @@ class SourceEntry(QVBoxLayout):
 		self.authors = kwargs["authors"]
 		self.tags = kwargs["tags"]
 		self.desc = kwargs["desc"]
+		self.path = kwargs["file_url"]
 
 		self.addWidget(QLabel(self.title))
-		self.addWidget(QLabel("By %s" % self.authors))
+		secondary_text = "By %s" % self.authors
+		secondary_text += "| "
+		secondary_text += self.tags
+		self.addWidget(QLabel(secondary_text))
+		PDF_link = QPushButton("PDF")
+		PDF_link.clicked.connect(self.open_pdf())
+		self.addWidget(PDF_link)
+		pdb.set_trace()
 		desc = QLabel(self.desc)
 		desc.setWordWrap(True)
 		self.addWidget(desc)
+
+	def open_pdf(self):
+		file_path = Paths.files_path + self.path
+		os.startfile(file_path)
+
 
 class App(QWidget):
 
@@ -68,7 +83,8 @@ class App(QWidget):
 			source_grid.addLayout(SourceEntry(title=data[i]["title"],
 											  authors=data[i]["authors"],
 											  tags=data[i]["tags"],
-											  desc=lorem_ipsum), i, 0)
+											  desc=lorem_ipsum, 
+											  file_url=data[i]["files"][0]["path"]), i, 0)
 		source_container.setLayout(source_grid)
 		scroll_area.setWidget(source_container)
 		return scroll_area
