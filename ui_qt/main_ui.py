0ed56782dd3603d51f29986e3520d1c1c4258ac1
akumar01 <akumar.4db@gmail.com>
2017-05-15 19:50:08 -0700

    Beginning work on new home page idea

--- ui_qt/main_ui.py
+++ ui_qt/main_ui.py
@@ -1,13 +1,27 @@
 import sys
 import os
+from math import ceil
 from win32api import GetSystemMetrics
 from PyQt5.QtWidgets import (QApplication, QVBoxLayout, QHBoxLayout, QWidget,
-							 QLabel, QPushButton, QScrollArea, QGridLayout)
+							 QLabel, QPushButton, QScrollArea, QGridLayout,
+							 QGroupBox, QStackedLayout)
 from crawler.agg.json_out import read_json
 from crawler.project_vars import Paths, Spiders
 import pdb
 
+active_sources = Spiders.spiders
+
 lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum tempor tortor, vitae molestie lorem mattis et. Mauris consectetur massa non metus blandit scelerisque vestibulum nec augue. Donec eu venenatis dui. Praesent consectetur facilisis justo, quis porta odio convallis sit amet. Praesent congue quam eros, malesuada feugiat velit lacinia sit amet. Integer condimentum in nibh consectetur accumsan. Aliquam erat volutpat. Morbi eget vulputate dolor, quis malesuada nisl. Ut quis tincidunt purus, quis cursus augue. Vivamus vitae pellentesque elit, a sagittis nulla. Praesent varius, magna sit amet blandit porttitor, leo mauris sodales risus, varius vehicula quam elit vitae odio.'
+
+
+class SourceSelector(QLabel):
+
+	def __init__(self, **kwargs):
+		super().__init__()
+
+	def mouse_proess(self):
+		pass
+
 
 class SourceEntry(QVBoxLayout):
 
@@ -42,10 +56,17 @@ class App(QWidget):
 	def __init__(self):
 		super().__init__()
 
+		self.load_data()
+
 		self.init_window()
 
 		self.init_root_layout()
 
+
+	def load_data(self):
+		self.data = {}
+		for src in active_sources:
+			self.data["src"] = read_json.read_data(src)
 
 	def init_window(self):
 		
@@ -56,9 +77,18 @@ class App(QWidget):
 	def init_root_layout(self):
 		root_layout = QVBoxLayout()
 		topmenu = self.init_topmenu()
-		scroll_area = self.init_scrollarea()
+
+		content_area = QVBoxLayout()
+
+		sources = self.init_sources()
+
+		new_articles = self.init_new_articles()
+
+		content_area.addWidget(sources)
+		content_area.addWidget(scroll_area)
+
 		root_layout.addLayout(topmenu)
-		root_layout.addWidget(scroll_area)
+		root_layout.addLayout(content_area)
 		self.setLayout(root_layout)
 
 	def init_topmenu(self):
@@ -71,21 +101,58 @@ class App(QWidget):
 		topmenu.addWidget(Last_Updated)
 		return topmenu
 
+	# List of sources
+	def init_sources(self):
+		sources = QGroupBox("Sources")
+		sources_layout = QGridLayout()
+
+		n_columns = 3
+		n_rows = int(ceil(len(active_sources)/n_columns.))
+
+		for i in range(n_rows):
+			for j in range(n_columns):
+				ind = i * n_columns + j
+				if ind >= len(active_sources):
+					break
+				label = QLabel(active_sources[ind])
+				sources_layout.addWidge(label)
+
+		sources.setLayout(sources_layout)
+		return sources
+
+	# Newest stories
+	def init_new_articles(self):
+		new_articles = QGroupBox("At a Glance")
+		new_articles_layout = QGridLayout()
+
+		# Take the newest article from each source:
+		new_articles = []
+		for src in active_sources:
+			news_articles.append(self.data["src"][0])
+
+		n_columns = 3
+		n_rows
+
+
 	def init_scrollarea(self):
-		scroll_area = QScrollArea()
-		source_container = QWidget()
-		source_grid = QGridLayout()
+		source_stack = QStackedLayout()
 
-		data = read_json.read_data('nature_news')
+		for source in active_sources:
 
-		for i in range(len(data)):
-			source_grid.addLayout(SourceEntry(title=data[i]["title"],
-											  authors=data[i]["authors"],
-											  tags=data[i]["tags"],
-											  desc=lorem_ipsum, 
-											  file_url=data[i]["files"][0]["path"]), i, 0)
-		source_container.setLayout(source_grid)
-		scroll_area.setWidget(source_container)
+			scroll_area = QScrollArea()
+
+			source_grid = QGridLayout()
+
+			for i in range(len(data)):
+				source_grid.addLayout(SourceEntry(title=data[i]["title"],
+												  authors=data[i]["authors"],
+												  tags=data[i]["tags"],
+												  desc=lorem_ipsum, 
+												  file_url=data[i]["files"][0]["path"]), i, 0)
+
+
+			scroll_area.setLayout(source_grid)
+			source_stack.addWidget(scroll_area)
 		return scroll_area
 
 
